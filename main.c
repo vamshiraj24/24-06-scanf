4e2e5a406f1ec9603a48ad19305ef5de9469b1e6
VVAMSHIRAJVAMSH <83009372+vamshiraj24@users.noreply.github.com>
2021-06-17 11:30:04 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  int num_1, num_2, sum;
+  printf("enter number 1 ");scanf("%d", &num_1);
+  printf("enter number 2 ");scanf("%d", &num_2);
+
+
+
+ 
+  sum = num_1 + num_2;
+  
+  
+  printf("%d\n", sum);
+
+printf("num_1 = %d\nnum_2 = %d\n", num_1, num_2);
+printf("sum = %d\n\n\n", sum);
+  return 0;
+}
