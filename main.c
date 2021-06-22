9f1c42045c37fdb0a291a79189df9fbde1f63b19
VAMSHI  RAJ <83009372+vamshiraj24@users.noreply.github.com>
2021-06-22 13:49:34 +0000

    simplified one

--- main.c
+++ main.c
@@ -5,16 +5,10 @@ int main(void)
   int num_1, num_2, sum;
   printf("enter number 1 ");scanf("%d", &num_1);
   printf("enter number 2 ");scanf("%d", &num_2);
-
-
-
- 
+  
   sum = num_1 + num_2;
   
-  
-  printf("%d\n", sum);
-
-printf("num_1 = %d\nnum_2 = %d\n", num_1, num_2);
-printf("sum = %d\n\n\n", sum);
+  printf("num_1 = %d\nnum_2 = %d\n", num_1, num_2);
+  printf("sum = %d\n\n\n", sum);
   return 0;
 }
